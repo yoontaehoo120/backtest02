8bb4436fb9a977ddd27deb78e3b2d3e692bf940c
yoontaehoo120 <96024293+yoontaehoo120@users.noreply.github.com>
2022-01-19 17:52:11 +0900

    20220119

--- upbit_ticker_data_copy.py
+++ upbit_ticker_data_copy.py
@@ -16,34 +16,55 @@ class upbit_ticker_data:
     def ohlcv_base_is_now(self, count=1, base="00:00:00"):
         """60분봉으로 1일 ohlcv 구하기,
          데이터 전처리, 요청일이 8일 이상이면 8일씩 잘라서 가공 및 합치기 """
+        global df
+        df_list = []
         list_NewDf = []  # list_NewDf 는 df의 임시저장소, 전역변수
+        print("count: %d" % count)
         if base == "now" or base == "NOW" or base == "Now":  # 시간인자 now 이면 현재시각 기준
-            base = datetime.datetime(self.now.year,  # 해당 시간 0분으로 셋팅
-                                     self.now.month, self.now.day,
-                                     self.now.hour)
-            df = pyupbit.get_ohlcv(self.ticker, interval="minute60",
-                                   count=count * 24, to=base, period=1)
+            end_datetime = datetime.datetime(self.now.year,  # 해당 시간 0분으로 셋팅
+                                             self.now.month, self.now.day,
+                                             self.now.hour)
+            start_datetime = end_datetime - datetime.timedelta(count)  # 시작일자 설정
+            print("start_datetime", start_datetime)
+            print("end_datetime", end_datetime)
+
+            for i in range(count, 0, -1):
+                print("\ni = %d" % i)
+                to_datetime = end_datetime - datetime.timedelta(i)
+                print("to_datime: ", to_datetime)
+                df = pyupbit.get_ohlcv(self.ticker, interval="minute60",
+                                       count=24, to=to_datetime)
+
+                df_list.append(df)
+                time.sleep(0.1)
 
         else:
-            start_date = datetime.datetime(self.now.year,  # 해당 시간 0분으로 셋팅
-                                           self.now.month, self.now.day)
-            print("in else, start_date", start_date)
-            tttt = f'{start_date}'
-            print("tttt", tttt)
-            df = pyupbit.get_ohlcv(self.ticker, interval="minute60",
-                                   count=count * 24, to=f'{start_date}', period=1)
 
-        print("count*24: %d" % (count * 24))  # 임시
-        print("df: \n", df)  # 임시
-        print("base", base)  # 임시
-        df.to_excel("a.xlsx")  # 임시
-        df['high'] = df['high'].rolling(24).max()
-        df['low'] = df['low'].rolling(24).min()
-        df['volume'] = df['volume'].rolling(24).sum()
+            end_datetime = datetime.datetime(self.now.year,  # 해당 시간 0분으로 셋팅
+                                             self.now.month, self.now.day,
+                                             self.now.hour)
+            start_datetime = end_datetime - datetime.timedelta(count)
+            print("start_datetime", start_datetime)
+            print("end_datetime", end_datetime)
+            for i in range(count, 0, -1):
+                print("\ni = %d" % i)
+                to_datetime = end_datetime - datetime.timedelta(i)
+                to_datetime = to_datetime.strftime("%Y%m%d")  # 예)날짜를 20220120 포맷으로 변환
+                print("to_datime: ", to_datetime)
+                df = pyupbit.get_ohlcv(self.ticker, interval="minute60",
+                                       count=24, to=f'{to_datetime}' + " " + base)
 
-        df.to_excel("b.xlsx")  # 임시
-        count_a = count * 24
-        print("count_a = %d" % count_a)  # 임시
+                df_list.append(df)
+                time.sleep(0.1)
+
+        df_total = pd.concat(df_list)
+        print(df_total)
+        df_total.to_excel("a.xlsx")  # 임시
+        df_total['high'] = df_total['high'].rolling(24).max()
+        df_total['low'] = df_total['low'].rolling(24).min()
+        df_total['volume'] = df_total['volume'].rolling(24).sum()
+
+        df_total.to_excel("b.xlsx")  # 임시
         for i in range(0, count_a, 24):
             i_delta = i + 23
             print("i = %d" % i)  # 임시
